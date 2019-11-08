a84a5d766182ba3ec094a2839a3689afeb8f6dfb
Laszlo Sandor <voidlaszlo@gmail.com>
2019-11-08 16:09:42 +0100

    adding blog component

--- src/app/travel/blog/blog.component.ts
+++ src/app/travel/blog/blog.component.ts
@@ -8,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class BlogComponent implements OnInit {
 
   constructor() { }
+  blogposts = [
+    {
+      author : "Laszlo",
+      body : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti molestias, fugiat culpa eius natus repellat?",
+      title : "Lorem he?"
+    },
+    {
+      author : "Kecske",
+      body : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti molestias, fugiat culpa eius natus repellat?",
+      title : "Lorem he?"
+    },
+    {
+      author : "Dorottya",
+      body : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti molestias, fugiat culpa eius natus repellat?",
+      title : "Lorem he?"
+    },
+  ]
 
   ngOnInit() {
   }
